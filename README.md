1c2b1c4bdca05a7513308fd65a33ea7ab46b312a
Thaissa Falbo <thaissa@thaissa.eng.br>
2017-09-10 01:55:06 -0300

    Some adjustments to run the program

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Udacity - FSND - P1
+
+This project is part of Udacity's Full Stack Nanodegree.
+
+
+## How to run...
+
+Execute python entertainment_center.py.
+The website will open in your browser
--- entertainment_center.py
+++ entertainment_center.py
@@ -33,8 +33,8 @@ hunger_games = media.Movie("Hunger Games",
 
 
 movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games ]
-#fresh_tomatoes.open_movies_page(movies)
+fresh_tomatoes.open_movies_page(movies)
 #print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
-print(media.Movie.__name__)
-print(media.Movie.__module__)
+#print(media.Movie.__doc__)
+#print(media.Movie.__name__)
+#print(media.Movie.__module__)
